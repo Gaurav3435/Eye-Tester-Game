09afff18d7009d471185e4625d5b021190becf0d
Gaurav3435 <gauravpatil22301@gmail.com>
2021-12-25 18:00:52 +0530

    Added GUI parts

--- function.py
+++ function.py
@@ -1,9 +1,53 @@
 from tkinter import *
+import random
+import os,sys
+
+def restart_program():
+    """Restarts the current program.
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function."""
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+def create_application_button(root):
+    quit_ = Button(root, text = 'Quit', width=20 ,command = root.destroy)
+    quit_.pack(side=BOTTOM,pady=10)
+    change = Button(root, text = 'change', width=20,command =restart_program)
+    change.pack(side=BOTTOM,pady=10)
+    return root
+
+def create_frame(root):
+    frame = Frame(root, relief=RAISED, borderwidth=1)
+    frame.pack(fill=BOTH, expand=True)
+    return root
+
+def from_rgb(rgb):
+    return "#%02x%02x%02x" % rgb 
+
+def return_rgb():
+    r=random.randint(0,255)
+    g=random.randint(0,0)
+    b=random.randint(0,255)
+    return (r,g,b)
+
+def create_option_color(root,r=0,g=0,b=0):
+    diff_bar=[]
+    for i in range(9):
+        diff_bar.append(random.randint(0,255))
+        color_= Button (root, bg=from_rgb((r,g+diff_bar[i],b)),width=10,height=5)
+        color_.pack(side=LEFT,pady=50,padx=10,anchor=CENTER)
+    print(diff_bar)
+    return root
+
+def display_main_color(root,r=0,g=0,b=0):
+    canvas = Canvas(root, width = 200, height = 200,bg=from_rgb((r, g, b)))  
+    canvas.pack(side=TOP)
+    return root
 
 def donothing():
-   filewin = Toplevel(root)
-   button = Button(filewin, text="Do nothing button")
-   button.pack(side=tk.BOTTOM)
+    filewin = Toplevel(root)
+    button = Button(filewin, text="Do nothing button")
+    button.pack(side=BOTTOM)
 
 def menu_bar(root):
     menubar = Menu(root)
@@ -33,7 +77,7 @@ def menu_bar(root):
     return root
 
 def create_image(root):
-    a=0
+    
     return root
 
 def main_title(root,size=20,type_="Arial"):
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from tkinter import *
-from function import donothing,menu_bar,main_title,create_image
+from function import donothing,menu_bar,main_title,display_main_color,create_option_color,return_rgb,create_frame,create_application_button
+global root
 
 root =Tk()
 root.title("Eye Test")
 root.geometry("900x650")
+root.maxsize(900, 700)
+root.minsize(900, 600)
 root=menu_bar(root)
 root=main_title(root,18,"Roman")
-root=create_image(root)
+rgb=return_rgb()
+print(rgb[0],rgb[1],rgb[2])
+root=display_main_color(root,rgb[0],rgb[1],rgb[2])
+root=create_application_button(root)
+root=create_frame(root)
+root=create_option_color(root,rgb[0],rgb[1],rgb[2])
 root.mainloop()
